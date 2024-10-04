6458aafdd86a728f6ebebdd286bb3d97b9773152
medtik <longpham6595@gmail.com>
2024-10-04 23:08:21 +0700

    Update file đề bài cho 2/5 test

---
+++ CLAOJ/OngNgauBaNgau/solveSimulUpgrage.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+// SPEED UP
+// #pragma GCC optimize("O3") //how good is this? lol
+// #pragma GCC target("avx,avx2,sse,sse2,sse3,sse4,popcnt,fma")
+// #pragma GCC optimize("unroll-loops")
+#define fast_io                   \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+//============================================================================
+// START PROGRAM
+//============================================================================
+long long n, m, s, t;
+
+int main()
+{
+    fast_io;
+    cin >> n >> m >> s >> t;
+
+    // getchar();
+}
