8fafefa7fe82a15fec0e1894d506d4d7fb8f3d77
medtik <longpham6595@gmail.com>
2024-10-09 12:39:28 +0700

    Hoàn thành xong 4/5 bài
    Hoàn thành toàn bộ test cases cho 4/5 bài
    Một số lưu ý về thời gian chạy:
    Bài 1: Giới hạn thời gian: 1s; GIới hạn bộ nhớ: 512MB
    Bài 2: Giới hạn thời gian: 1s; Giới hạn bộ nhớ: 512MB
    Bài 3: Giới hạn thời gian: 1s; Giới hạn bộ nhớ: 512MB
    Bài 4: Giới hạn thời gian: 1s; Giới hạn bộ nhớ: 512MB

--- RealTests/[DucHoa]VongTruong20242025/Bai4/Solution/gentest.py
+++ RealTests/[DucHoa]VongTruong20242025/Bai4/Solution/gentest.py
@@ -6,14 +6,15 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(script_dir, "genPy.in")
 
 # 1 <= n,q <= 100000
+# 1 <= x <= 1000000
 n = 100000
 q = 100000
 with open(file_path, "w") as f:
     f.write(f"{n} \n")
     for i in range(n):
-        x = random.randint(100000, 1000000)
+        x = random.randint(10000, 100000)
         f.write(f"{x} \n")
     f.write(f"{q} \n")
     for i in range(q):
-        m = random.randint(100000000, 999999999)
+        m = random.randint(100000, 999999999)
         f.write(f"{m} \n")
